2c17e5be502bd2fbfed6dc15ea430b38ea780d14
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-04-27 15:44:42 +1200

    A buch of shite
    Stuff adjectives, thing categories, description fixes, added source files, moved some stuff around a bit.

---
+++ 1.3/Source/ESCP_ThingCatching/ESCP_ThingCatching/ESCP_ThingCatching/CatchableOutputDef.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace ESCP_ThingCatching
+{
+    public class CatchableOutputDef : Def
+    {
+        public ThingDef outputThingDef;
+        public IntRange outputRange = new IntRange(1, 15);
+        public float chance = 1f;   //1 is assumed to be the default value, anything excessively larger may cause issues
+        public List<string> biomes;
+        public bool isSpecial = false;
+        public List<string> catchTags;
+
+    }
+}
