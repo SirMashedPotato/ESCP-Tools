1dffa2ed81818e03903db92d2f977e1cd81cb7c3
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-05-03 10:38:11 +1200

    Hediff on death spawner

---
+++ 1.3/Source/ESCP_ThingSpawner/ESCP_ThingSpawner/ThingSpawner/HediffCompProperties_PawnSpawnerOnDeath.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Verse;
+
+namespace ESCP_ThingSpawner
+{
+    class HediffCompProperties_PawnSpawnerOnDeath : HediffCompProperties
+    {
+        public HediffCompProperties_PawnSpawnerOnDeath()
+        {
+            this.compClass = typeof(HediffComp_PawnSpawnerOnDeath);
+        }
+    }
+}
---
+++ 1.3/Source/ESCP_ThingSpawner/ESCP_ThingSpawner/ThingSpawner/HediffComp_PawnSpawnerOnDeath.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace ESCP_ThingSpawner
+{
+    class HediffComp_PawnSpawnerOnDeath : HediffComp
+    {
+		public HediffCompProperties_PawnSpawnerOnDeath Props
+		{
+			get
+			{
+				return (HediffCompProperties_PawnSpawnerOnDeath)this.props;
+			}
+		}
+
+
+        public override void Notify_PawnKilled()
+        {
+            var props = CreateAtProperties.Get(this.Def);
+            if (props != null && props.kindToSpawn != null)
+            {
+                int target = props.numberToSpawn;
+                for (int i = 0; i < target; i++)
+                {
+                    PawnSpawnerUtility.PawnSpawner(props, null, base.parent.pawn);
+                }
+            }
+
+            base.Notify_PawnKilled();
+        }
+    }
+}
