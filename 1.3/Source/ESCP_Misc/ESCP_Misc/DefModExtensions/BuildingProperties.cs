a4c5cf6404cad98aa19a1d6eedab5bf03adbc772
SirMashedPotato <88062719+SirMashedPotato@users.noreply.github.com>
2022-05-27 09:54:09 +1200

    Modified canPlaceAt Harmony patch
    Uses a mod extension, can't place things with preventDuplicatePlacement on things with preventDuplicatePlacement.

---
+++ 1.3/Source/ESCP_Misc/ESCP_Misc/DefModExtensions/BuildingProperties.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace ESCP_Misc
+{
+    class BuildingProperties : DefModExtension
+    {
+
+        public bool preventDuplicatePlacement = false;
+
+        public static BuildingProperties Get(Def def)
+        {
+            return def.GetModExtension<BuildingProperties>();
+        }
+    }
+}
--- 1.3/Source/ESCP_Misc/ESCP_Misc/Harmony/CanPlaceBlueprintAt.cs
+++ 1.3/Source/ESCP_Misc/ESCP_Misc/Harmony/CanPlaceBlueprintAt.cs
@@ -14,16 +14,19 @@ namespace ESCP_Misc
     {
         public static void Postfix(ref AcceptanceReport __result, BuildableDef entDef, IntVec3 center, Rot4 rot, Map map, bool godMode = false, Thing thingToIgnore = null, Thing thing = null, ThingDef stuffDef = null)
         {
-            foreach (IntVec3 item in GenAdj.OccupiedRect(center, rot, entDef.Size))
+            if (BuildingProperties.Get(entDef) != null && BuildingProperties.Get(entDef).preventDuplicatePlacement)
             {
-                List<Thing> thingList = item.GetThingList(map);
-                for (int i = 0; i < thingList.Count; i++)
+                foreach (IntVec3 item in GenAdj.OccupiedRect(center, rot, entDef.Size))
                 {
-                    Thing thing2 = thingList[i];
-                    if (thing2 != thingToIgnore && (thing2.def.building != null || thing2.def.entityDefToBuild != null) && ((entDef is ThingDef thingDef && thingDef.building != null && !thingDef.building.isEdifice && !thing2.def.building.isEdifice) || (thing2.def.IsBlueprint && thing2.def.entityDefToBuild != null && thing2.def.entityDefToBuild is ThingDef thingDef2 && thingDef2.building != null && !thingDef2.building.isEdifice)))
+                    List<Thing> thingList = item.GetThingList(map);
+                    for (int i = 0; i < thingList.Count; i++)
                     {
-                        __result = new AcceptanceReport("SpaceAlreadyOccupied".Translate());
-                        return;
+                        Thing thing2 = thingList[i];
+                        if (BuildingProperties.Get(thing2.def) != null && BuildingProperties.Get(thing2.def).preventDuplicatePlacement)
+                        {
+                            __result = new AcceptanceReport("SpaceAlreadyOccupied".Translate());
+                            return;
+                        }
                     }
                 }
             }
